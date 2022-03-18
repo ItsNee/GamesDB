09b0931ac944927154864cc2747508fdb4a843d0
Tang Zhi Xiang <zifengtang540@gmail.com>
2022-03-19 00:42:28 +0800

    Update individualGames.php
    - details of selected game will now show
    - some ideas for improvement are listed in comments

--- GamesDb/individualGames.php
+++ GamesDb/individualGames.php
@@ -10,44 +10,65 @@
 
         <?php
         $appId = $_POST['appId'];
+
+        // obtained from https://startbootstrap.com/snippets/portfolio-item
+
+        $query = "SELECT * FROM games where appid = " . $appId;
+        $result = mysqli_query($conn, $query);
+        if ($result->num_rows > 0) {
+            
+            while ($row = $result->fetch_assoc()) {
+                echo '<header class="masthead">';
+                echo '<div class="container">';
+
+                $gameGenre = $row["gameGenre"];
+                $appId = $row["appid"];
+                $name = $row["name"];
+                $developer = $row["developer"];
+                $positiveVotes = $row["positiveVotes"];
+                $negativeVotes = $row["negativeVotes"];
+                $price = $row["price"];
+                $gameImage = $row["gameImage"];
+
+                echo '<h1 class="my-4">' . $name . '</h1>';
+                echo '<div class="row">';
+                echo '<div class = "col-md-8">';
+                echo '<img class = "img-fluid" width="100%" src = "' . $gameImage . '" alt = "Image of '.$name.'">';
+                echo '</div>';
+                echo '<div class = "col-md-4">';
+                echo '<h3 class = "my-3">Description</h3>';
+                echo '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae. Sed dui lorem, adipiscing in adipiscing et, interdum nec metus. Mauris ultricies, justo eu convallis placerat, felis enim.</p>';
+                echo '<h5 class = "my-3">Genre : ' . ucfirst($gameGenre) . '</h5>';
+                // idea for votes: show a line graph and plot green and red lines on x axis (e.g. <--------green---------><-----red----->) 
+                echo '<h5 class = "my-3">Positive Votes : ' . $positiveVotes . '</h5>';
+                echo '<h5 class = "my-3">Negative Votes : ' . $negativeVotes . '</h5>';
+                echo '<h5 class = "my-3">Stars : blah blah</h5>'; // idea: rate it upon 5 stars
+                echo '<h5 class = "my-3">Developer : ' . $developer . '</h5>';
+                echo '<br>';
+                echo '<div class = "card-footer p-4 pt-0 border-top-0 bg-transparent">';
+                echo '<form id = "addToCartForm" name = "addToCartForm" action = "cart.php" method = "POST" enctype = "multipart/form-data">';
+                echo '<input type = "hidden" name = "appId" value = "' . $appId . '" />';
+                echo '<div class = "text-center">';
+                echo '<p>$'.$price.'</p>'; // price to be side by side with add to cart button
+                echo '<button class = "btn btn-outline-primary mt-auto" type = "submit">Add to Cart!</button></div>';
+                echo '</form>';
+                echo '</div>';
+                echo '</div>';
+                echo '</div>';
+                echo '</div>';
+                echo '</header>';
+                echo '</div>';
+                echo '</div>';
+                echo '</section>';
+            }
+        } else {
+            echo "0 results";
+        }
         ?>
-
-        <!--obtained from https://startbootstrap.com/snippets/portfolio-item-->
-        <header class='masthead'>
-            <div class="container">
-                <h1 class="my-4">Game title</h1>
-                <div class="row">
-                    <div class="col-md-8">
-                        <img class="img-fluid" src="game image" alt="">
-                    </div>
-
-                    <div class="col-md-4">
-                        <h3 class="my-3">Description</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae. Sed dui lorem, adipiscing in adipiscing et, interdum nec metus. Mauris ultricies, justo eu convallis placerat, felis enim.</p>
-                        <h5 class="my-3">Genre : 'blah blah'</h5>
-                        <h5 class="my-3">Positive Votes : 'blah blah'</h5>
-                        <h5 class="my-3">Negative Votes : 'blah blah'</h5>
-                        <h5 class="my-3">Stars : 'blah blah'</h5>
-                        <br>
-                        <div class = "card-footer p-4 pt-0 border-top-0 bg-transparent">
-                            <form id="addToCartForm" name="addToCartForm" action="cart.php" method="POST" enctype="multipart/form-data">
-                                <input type="hidden" name="appId" value="' . $appId . '" />
-                                <input type="hidden" name="username" value="' . $username . '" />
-                                <div class = "text-center"><button class = "btn btn-outline-primary mt-auto" type="submit">Add to Cart!</button></div>
-                            </form>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-        </header>
-
 
         <?php
         include "footer.inc.php";
         ?>
-
-
 
         <!-- Bootstrap core JS-->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
