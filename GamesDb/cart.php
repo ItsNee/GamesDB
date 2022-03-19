93d7f8fa46ae869f1c896709b6f39c2ade65567a
djmeowmix25 <kelly.loh2001@gmail.com>
2022-03-20 02:59:04 +0800

    takes data from db now

--- GamesDb/cart.php
+++ GamesDb/cart.php
@@ -7,139 +7,119 @@
         <?php
         include "navPostLogin.inc.php";
         ?>
-        <!-- Masthead header-->
-        <header class="masthead">
-            <div class="container px-5">
-                <div class="row gx-5 align-items-center">
-                    <div class="col-lg-6">
-                        <!-- Mashead text and app badges-->
-                        <div class="mb-5 mb-lg-0 text-center text-lg-start">
-                            <h1 class="display-1 lh-1 mb-3">This is another page lol!</h1>
-                            <p class="lead fw-normal text-muted mb-5">Get all your games from one centralized platform!</p>
-                            <div class="d-flex flex-column flex-lg-row align-items-center">
-                                <a class="me-lg-3 mb-4 mb-lg-0" href="#!"><img class="app-badge" src="assets/img/google-play-badge.svg" alt="..." /></a>
-                                <a href="#!"><img class="app-badge" src="assets/img/app-store-badge.svg" alt="..." /></a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-6">
-                        slatt
-                        <!-- Masthead device mockup feature-->
-                        <div class="masthead-device-mockup">
-                            <svg class="circle" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-                                <defs>
-                                    <linearGradient id="circleGradient" gradientTransform="rotate(45)">
-                                        <stop class="gradient-start-color" offset="0%"></stop>
-                                        <stop class="gradient-end-color" offset="100%"></stop>
-                                    </linearGradient>
-                                </defs>
-                                <circle cx="50" cy="50" r="50"></circle></svg
-                            ><svg class="shape-1 d-none d-sm-block" viewBox="0 0 240.83 240.83" xmlns="http://www.w3.org/2000/svg">
-                                <rect x="-32.54" y="78.39" width="305.92" height="84.05" rx="42.03" transform="translate(120.42 -49.88) rotate(45)"></rect>
-                                <rect x="-32.54" y="78.39" width="305.92" height="84.05" rx="42.03" transform="translate(-49.88 120.42) rotate(-45)"></rect></svg
-                            ><svg class="shape-2 d-none d-sm-block" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg"><circle cx="50" cy="50" r="50"></circle></svg>
-                            <img class="floatAboveEverything" src="assets/img/pcSetup.png" alt="Gaming Setup">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </header>
-        <!-- Quote/testimonial aside-->
-        <aside class="text-center bg-gradient-primary-to-secondary">
-            <div class="container px-5">
-                <div class="row gx-5 justify-content-center">
-                    <div class="col-xl-8">
-                        <div class="h2 fs-1 text-white mb-4">"This is another page lol!"</div>
-                        <img src="assets/img/tnw-logo.svg" alt="..." style="height: 3rem" />
-                    </div>
-                </div>
-            </div>
-        </aside>
-        <!-- App features section-->
-        <section id="aboutUs">
-            <div class="container px-5">
-                <div class="row gx-5 align-items-center">
-                    <div class="col-lg-8 order-lg-1 mb-5 mb-lg-0">
-                        <div class="container-fluid px-5">
-                            <div class="row gx-5">
-                                <div class="col-md-6 mb-5">
-                                    <!-- Feature item-->
-                                    <div class="text-center">
-                                        <i class="bi bi-controller icon-feature text-gradient d-block mb-3"></i>
-                                        <h3 class="font-alt">Title 1</h3>
-                                        <p class="text-muted mb-0">Need to bs. But lets work on functionality first -Nee</p>
-                                    </div>
-                                </div>
-                                <div class="col-md-6 mb-5">
-                                    <!-- Feature item-->
-                                    <div class="text-center">
-                                        <i class="bi bi-joystick icon-feature text-gradient d-block mb-3"></i>
-                                        <h3 class="font-alt">Title 2</h3>
-                                        <p class="text-muted mb-0">Need to bs. But lets work on functionality first -Nee</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-md-6 mb-5 mb-md-0">
-                                    <!-- Feature item-->
-                                    <div class="text-center">
-                                        <i class="bi bi-dice-5 icon-feature text-gradient d-block mb-3"></i>
-                                        <h3 class="font-alt">Title 3</h3>
-                                        <p class="text-muted mb-0">Need to bs. But lets work on functionality first -Nee</p>
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <!-- Feature item-->
-                                    <div class="text-center">
-                                        <i class="bi-patch-check icon-feature text-gradient d-block mb-3"></i>
-                                        <h3 class="font-alt">Title 4</h3>
-                                        <p class="text-muted mb-0">Need to bs. But lets work on functionality first -Nee</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-4 order-lg-0">
-                        <!-- Features section device mockup-->
-                        <div class="features-device-mockup">
-                            <svg class="circle" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-                                <defs>
-                                    <linearGradient id="circleGradient" gradientTransform="rotate(45)">
-                                        <stop class="gradient-start-color" offset="0%"></stop>
-                                        <stop class="gradient-end-color" offset="100%"></stop>
-                                    </linearGradient>
-                                </defs>
-                                <circle cx="50" cy="50" r="50"></circle></svg
-                            ><svg class="shape-1 d-none d-sm-block" viewBox="0 0 240.83 240.83" xmlns="http://www.w3.org/2000/svg">
-                                <rect x="-32.54" y="78.39" width="305.92" height="84.05" rx="42.03" transform="translate(120.42 -49.88) rotate(45)"></rect>
-                                <rect x="-32.54" y="78.39" width="305.92" height="84.05" rx="42.03" transform="translate(-49.88 120.42) rotate(-45)"></rect></svg
-                            ><svg class="shape-2 d-none d-sm-block" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg"><circle cx="50" cy="50" r="50"></circle></svg>
-                            <img class="floatAboveEverything" src="assets/img/gta5Cover.jpeg" width="100%" alt="GTA5 Cover Pic">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <!-- Basic features section-->
-        <section class="bg-light">
-            <div class="container px-5">
-                <div class="row gx-5 align-items-center justify-content-center justify-content-lg-between">
-                    <div class="col-12 col-lg-5">
-                        <h2 class="display-4 lh-1 mb-4">Enter a new age of Gaming</h2>
-                        <p class="lead fw-normal text-muted mb-5 mb-lg-0">Someone please help me write some bs here -Nee</p>
-                    </div>
-                    <div class="col-sm-8 col-md-6">
-                        <div class="px-5 px-sm-0"><img class="img-fluid rounded-circle" src="https://source.unsplash.com/u8Jn2rzYIps/900x900" alt="..." /></div>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <?php
-        include "footer.inc.php";
-        ?>
-        
 
-        
+        <?php
+        include "db.inc.php";
+        $stmt = $conn->prepare("SELECT * FROM cart WHERE username=?");
+        $stmt->bind_param("s", $username);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        if ($result->num_rows > 0) {
+            $totalValue = 0;
+            echo '<section class="h-100">';
+            echo '<div class="container h-100 py-5">';
+            echo '<div class="row d-flex justify-content-center align-items-center h-100">';
+            echo '<div class="col-10">';
+            echo '<div class="d-flex justify-content-between align-items-center mb-4">';
+            echo '<h3 class="fw-normal mb-0 text-black">Shopping Cart</h3>';
+            echo '</div>';
+            while ($row = $result->fetch_assoc()) {
+                $appid = $row["appid"];
+                $qty = $row["qty"];
+
+                $stmt2 = $conn->prepare("SELECT * FROM games WHERE appid=?");
+                $stmt2->bind_param("s", $appid);
+                $stmt2->execute();
+                $result2 = $stmt2->get_result();
+                $row2 = $result2->fetch_assoc();
+                
+                $gameGenre = $row2["gameGenre"];
+                $name = $row2["name"];
+                $developer = $row2["developer"];
+                $price = $row2["price"];
+                $gameImage = $row2["gameImage"];
+                $totalValue = $totalValue + $price;
+                
+            echo '<div class="card rounded-3 mb-4">';
+            echo '<div class="card-body p-4">';
+            echo '<div class="row d-flex justify-content-between align-items-center">';
+            echo '<div class="col-md-2 col-lg-2 col-xl-2">';
+            echo '<img src="'.$gameImage.'"';
+            echo 'class="img-fluid rounded-3" alt="'.$name.'">';
+            echo '</div>';
+            echo '<div class="col-md-3 col-lg-3 col-xl-3">';
+            echo '<p class="lead fw-normal mb-2">'.$name.'</p>';
+            echo '<p><span class="text-muted">Genre: </span>'.$gameGenre.'</p>';
+            echo '</div>';
+            echo '<div class="col-md-3 col-lg-3 col-xl-2 d-flex">';
+            echo '<input id="form1" min="0" name="quantity" value="'.$qty.'" type="number"';
+            echo 'class="form-control form-control-sm" />';
+            echo '';
+            echo '<button class="btn btn-link px-2">';
+            echo 'Update';
+            echo '</button>';
+            echo '</div>';
+            echo '<div class="col-md-3 col-lg-2 col-xl-2 offset-lg-1">';
+            echo '<h5 class="mb-0">$'.$price.'</h5>';
+            echo '</div>';
+            echo '<div class="col-md-1 col-lg-1 col-xl-1 text-end">';
+            echo '<a href="#!" class="text-danger"><i class="fas fa-trash fa-lg"></i></a>';
+            echo '</div>';
+            echo '</div>';
+            echo '</div>';
+            echo '</div>';
+
+            }
+            
+            echo '<div class="card">';
+            echo '<div class="card-body">';
+            echo '<button type="button" class="btn btn-warning btn-block btn-lg">Proceed to Pay</button>';
+            echo '</div>';
+            echo '</div>';
+            echo '</div>';
+            echo '</div>';
+            echo '</div>';
+            echo '</section>';
+        } else {
+            echo '<header style ="height:100%;" class="masthead">';
+            echo '<div class="container px-5 text-center">';
+            echo '<div class="row gx-14 align-items-center">';
+            echo '<div class="col-lg-14">';
+            echo '<div class="mb-5 mb-lg-0 text-center text-lg-start">';
+            echo "<h1 class=\"display-4 lh-1 mb-3 text-center\">Your cart is empty!</h1>";
+            echo "<p class=\"lead fw-normal text-muted mb-5 text-center\"><a href=\"games.php\"> Browse games here </a></p>";
+            echo '</div>';
+            echo '</div>';
+            echo '</div>';
+            echo '</div>';
+            echo '</header>';
+        }
+        ?>
+
+
+
+
+
+
+        <!--        <header style ="height:100%;" class="masthead">
+                    <div class="container px-5 text-center">
+                        <div class="row gx-14 align-items-center">
+                            <div class="col-lg-14">
+                                <div class="mb-5 mb-lg-0 text-center text-lg-start">
+                                    <h1 class="display-4 lh-1 mb-3 text-center">Your cart is empty!</h1>
+                                    <p class="lead fw-normal text-muted mb-5 text-center"><a href="games.php"> Browse games here </a></p>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </header>-->
+<?php
+include "footer.inc.php";
+?>
+
+
+
         <!-- Bootstrap core JS-->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
         <!-- Core theme JS-->
